6e5d93950a6c39b3546e6703a83d6ba5465aedad
Jordana Sara Silva dos Santos <jordana.sara@academico.ifrn.edu.br>
2022-07-05 16:21:16 -0300

    Mexendo com a movimentaçãoi do player

--- New Unity Project (3)/Assets/Scripts/PlayerController.cs
+++ New Unity Project (3)/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-   public float moveSpeed; 
+   //public int coins = 0; 
+   
+   public float moveSpeed;
+   public float maxVelocity;
    
    
    private Gamecontrol _gameControl;
@@ -52,20 +55,64 @@ public class PlayerController : MonoBehaviour
 
    private void Move()
    {
+      // Pega o Vector que aponta na direção em que a camera está olhando e zeramos o componente Y
+      Vector3 camFoward = _mainCamera.transform.forward;
+      camFoward.y = 0; 
       // Calcula o movimento no eixo da camera para o movimento frente/tras
-      Vector3 moveVertical = _mainCamera.transform.forward * _moveInput.y;
+      Vector3 moveVertical =  camFoward * _moveInput.y;
 
+      // pega o vector que aponta para o lado direito da camera e zeramos componente Y
+      Vector3 camRight = _mainCamera.transform.right;
+      camRight.y = 0;
       // Calcula o movimento no eixo da camera para o movimento esquerda/direita
-      Vector3 moverHorizontal = _mainCamera.transform.right * _moveInput.x;
+      Vector3 moverHorizontal = camRight * _moveInput.x;
          
       // Adiciona a força no objeto atraves do rigidbody, com intensidade definida por moveSpeed
-      _rigidbody.AddForce((_mainCamera.transform.forward * _moveInput.y +
-                           _mainCamera.transform.right * _moveInput.x) *
-                          moveSpeed * Time.fixedDeltaTime);
+      _rigidbody.AddForce((moveVertical + moverHorizontal) * moveSpeed * Time.fixedDeltaTime);
    }
 
    private void FixedUpdate()
    {
       Move();
+      LimiteVelocity();
    }
+
+   private void LimiteVelocity()
+   {
+      //pegar a velocidade do player
+      Vector3 velocity = _rigidbody.velocity;
+
+      // checar se a velocidade está dentro dos limites nos diferentes eixos
+      // limitando o eixo x usando ifs, Abs e Sign
+      if (Mathf.Abs(velocity.x) > maxVelocity) velocity.x = Mathf.Sign(velocity.x) * maxVelocity;
+      
+      // -maxVelocity < velocity.z < maxVelocity
+      velocity.z = Mathf.Clamp(value: velocity.z, -maxVelocity, maxVelocity);
+
+      // alterar a velocidade do player para ficar dentro dos limites 
+      _rigidbody.velocity = velocity;
+   }
+   
+   /*
+    * 1 - Checar se o jogador está no chão
+    * --
+    * 2 - Jogador preciso apertar o botão de pulo
+    * 3 - Realizar o pulo através da física
+   */
+
+   private void OnDrawGizmos()
+   {
+      //Debug.DrawRay(start:transform.position, dir:Vector3.down * rayDistance );
+   }
+   
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.CompareTag("Coin"))
+      {
+         coins++;
+         Destroy(other.gameObject);
+      }
+   }
+   
 }
